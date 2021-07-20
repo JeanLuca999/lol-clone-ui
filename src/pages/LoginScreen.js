78cb3bb6bed84bd9b17f51905d64da625f0f1d03
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-07-20 14:35:02 -0300

    adicionando logo e botão de manter login

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -4,27 +4,45 @@ import LoginScreenWallpaper from '../assets/images/lol-wallpaper.svg'
 import facebookIcon from '../assets/images/facebook-icon.svg';
 import googleIcon from '../assets/images/google-icon.svg';
 import appleIcon from '../assets/images/apple-icon.svg';
+import lolLogo from '../assets/images/lol-logo.svg';
+
+import { useState } from "react";
 
 
 function LoginScreen() {
+
+    const [keepLogged, setKeepLogged] = useState(false)
+
+    function handleKeepLogged() {
+        setKeepLogged(!keepLogged)
+    }
+
     return(
         <WindowContainer>
             <Wrapper>
+
                 <ElementsContainer>
+                    <LolLogo src={lolLogo}></LolLogo>
                     <Title>Fazer login</Title>
                     <Input type="text" placeholder="NOME DE USUÁRIO" content="NOME DE USUÁRIO"></Input>
                     <Input type="password" placeholder="SENHA" content="SENHA"></Input>
+
                     <IconsWrapper>
                         <Icon>
-                          <img src={facebookIcon}></img>
+                          <img src={facebookIcon} alt="facebook"></img>
                         </Icon>
                         <Icon>
-                            <img src={googleIcon}></img>
+                            <img src={googleIcon} alt="google"></img>
                         </Icon>
                         <Icon>
-                            <img src={appleIcon}></img>
+                            <img src={appleIcon} alt="apple"></img>
                         </Icon>
                     </IconsWrapper>
+
+                    <KeepLogged>
+                        <CheckBox keepLogged={keepLogged} onClick={handleKeepLogged}></CheckBox> Manter login
+                    </KeepLogged>
+
                 </ElementsContainer>
                 <BackgroundContainer>
 
@@ -35,6 +53,13 @@ function LoginScreen() {
 }
 
 export default LoginScreen;
+
+
+const LolLogo = styled.img`
+    object-fit: contain;
+    object-position: center;
+    margin-bottom: 70px;
+`
 
 
 const Wrapper = styled.div`
@@ -113,16 +138,17 @@ const Input = styled.input`
 `
 
 const IconsWrapper = styled.div`
-    width: 288px;
+    width: 285px;
     display: flex;
+    margin-top: 14px;
 `
 
 const Icon = styled.div`
     flex-grow: 1;
     background-color: black;
     text-align: center;
-    padding: 5px;
-    margin-left: 10px;
+    padding: 5px 0;
+    margin-left: 5px;
     border-radius: 5px;
     display: flex;
     justify-content: center;
@@ -156,5 +182,47 @@ const Icon = styled.div`
     img {
         width: 20px;
         height: 20px;
+    }
+`
+
+const KeepLogged = styled.div`
+    width: 288px;
+    margin-top: 10px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 12px;
+    
+    font-weight: 800;
+    letter-spacing: 0.5px;
+    color: rgb(153,153,178);
+
+    &:hover {
+        color: rgb(126,126,126);
+    }
+`
+
+const CheckBox = styled.button`
+    width: 17px;
+    height: 17px;
+    background-color: ${ props => props.keepLogged ? 'rgb(209,54,57)' : 'rgb(237,237,237)'};
+    border: 0;
+    outline: 0;
+    cursor: pointer;
+    margin-right: 5px;
+    position: relative;
+
+    &::after {
+        content: '✓';
+        color: white;
+        font-size: 12px;
+        font-weight: bold;
+        text-align: center;
+        position: absolute;
+        width: 5px;
+        height: 5px;
+        top: 0;
+        left: 3px;
+        display: ${ props => props.keepLogged ? 'block' : 'none'};
     }
 `
